994a50114dfe0282fc07b8fb7237ddc88124bd60
Yash Raj <49111663+Ryash13@users.noreply.github.com>
2020-08-18 15:26:27 +0530

    Create longestConsicutiveSequence.cpp

---
+++ longestConsicutiveSequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> s(nums.begin(),nums.end());
+        int ans = 0;
+        int last = *s.begin() - 1 , len=0;
+        for(auto i:s)
+        {
+            if(i-last == 1)
+                ans = max(ans , ++len);
+            else
+                len = 1;
+            last = i;
+        }
+        return ans;
+    }
+};
