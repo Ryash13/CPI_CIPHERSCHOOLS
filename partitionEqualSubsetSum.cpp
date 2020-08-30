b8768464ca019c3604664ceeb571036a3f585442
Yash Raj <49111663+Ryash13@users.noreply.github.com>
2020-08-30 14:55:06 +0530

    Create partitionEqualSubsetSum.cpp

---
+++ partitionEqualSubsetSum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+            sum+=nums[i];
+        if(sum%2!=0) 
+            return false;
+        else
+        {
+            sum=sum/2;
+            bool t[n+1][sum+1];
+            for(int i=0;i<sum+1;i++) t[0][i]=false;
+            for(int i=0;i<n+1;i++) t[i][0]=true;
+            
+            for(int i=1;i<n+1;i++){
+                for(int j=1;j<sum+1;j++){
+                    if(nums[i-1]<=j)
+                        t[i][j]= (t[i-1][j] || t[i-1][j-nums[i-1]]);
+                    else 
+                        t[i][j]=t[i-1][j];
+                }
+            }
+            return t[n][sum];
+        }
+    }
+};
